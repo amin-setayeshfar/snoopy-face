23e1b8bc1055d1b834673a5f4d2b2961a61c5c6c
amin <amin.setayeshfar@gmail.com>
2021-10-04 12:49:26 +0330

    refactor: :art: some refactoring and adding the landmark toglle functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 declare var faceapi: any;
 
 @Component({
@@ -7,37 +7,41 @@ declare var faceapi: any;
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'faceapi';
+  public video: any;
+  public glassesLeft: any;
+  public glassesTop: any;
+  public glassesWidth: number;
+  public faceDetected: boolean = false;
+  public drawFaceLandmarks: boolean = true;
+  screenshot: any;
+  //TODO: adding joint
+  // public showJoint: boolean = true;
 
-  video: any;
-  public glassLeft: any;
-  public glassTop: any;
+  constructor(private changeDetaction: ChangeDetectorRef) {}
 
   ngOnInit() {
-    this.video = document.getElementsByTagName('video')[0];
+    //checking to make sure we are in a browser!
+    if (window && document) {
+      this.video = document.getElementsByTagName('video')[0];
+    }
     this.loadModels();
   }
 
   loadModels() {
-    // load the models
-    const MODEL_URL = '/assets/models/';
-
+    const modelsUrl = '/assets/models/';
+    // loading the models
     Promise.all([
-      faceapi.loadFaceLandmarkModel(MODEL_URL),
-      faceapi.loadFaceRecognitionModel(MODEL_URL),
-      faceapi.loadFaceExpressionModel(MODEL_URL),
-      faceapi.loadMtcnnModel(MODEL_URL),
+      faceapi.loadFaceLandmarkModel(modelsUrl),
+      faceapi.loadFaceRecognitionModel(modelsUrl),
+      faceapi.loadFaceExpressionModel(modelsUrl),
+      faceapi.loadMtcnnModel(modelsUrl),
     ]).then(() => {
-      console.log('models loaded!');
-      this.videoFaceDetection((x: any, y: any) => {
-        console.log(x, y);
-        this.glassLeft = x;
-        this.glassTop = y;
-      });
+      //models loaded!
+      this.videoFaceDetection();
     });
   }
 
-  videoFaceDetection(callback: any) {
+  videoFaceDetection() {
     let myNavigator = window.navigator as any;
     myNavigator.getUserMedia(
       { video: {} },
@@ -64,34 +68,79 @@ export class AppComponent {
               ?.getContext('2d')
               ?.clearRect(0, 0, canvas.width, canvas.height);
 
-            //check to see if there is a face!
+            //checking to see if there is a face!
             if (detections) {
               const resizedDetections = faceapi.resizeResults(
                 detections,
                 displaySize
               );
               faceapi.draw.drawDetections(canvas, resizedDetections);
-              faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+              if (this.drawFaceLandmarks) {
+                faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+              }
               faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
               const landmarks = detections['landmarks'];
               const leftEye = landmarks.getLeftEye();
               const rightEye = landmarks.getRightEye();
-              console.log('y ' + leftEye[0]['y']);
-              console.log('x ' + leftEye[0]['x']);
-              document.getElementsByTagName('img')[0].style.display = 'block';
-              document.getElementsByTagName('img')[0].style.left =
-                Math.round(leftEye[0]['x']) + 30 + 'px';
-              document.getElementsByTagName('img')[0].style.top =
-                Math.round(leftEye[0]['y']) + 10 + 'px';
-              document.getElementsByTagName('img')[0].width =
-                (rightEye[3]['x'] - leftEye[0]['x']) * 1.5;
+              this.changeDetaction.detectChanges();
+              this.faceDetected = true;
+              this.glassesLeft = Math.round(leftEye[0]['x']) + 30;
+              this.glassesTop = Math.round(leftEye[0]['y']) + 10;
+              this.glassesWidth = (rightEye[3]['x'] - leftEye[0]['x']) * 1.4;
             } else {
-              document.getElementsByTagName('img')[0].style.display = 'none';
+              this.faceDetected = false;
+              this.changeDetaction.detectChanges();
             }
-          }, 400);
+          }, 100);
         });
       },
       (err: any) => console.error(err)
     );
+  }
+
+  toggleFaceLandmarks() {
+    this.drawFaceLandmarks = !this.drawFaceLandmarks;
+  }
+
+  takeScreenshotStream() {
+    const width = screen.width * (window.devicePixelRatio || 1);
+    const height = screen.height * (window.devicePixelRatio || 1);
+
+    let stream;
+    const mediaStreamConstraints = {
+      audio: false,
+      video: {
+        width,
+        height,
+        frameRate: 1,
+      },
+    };
+
+    return (stream = (navigator.mediaDevices as any).getDisplayMedia(
+      mediaStreamConstraints
+    ));
+  }
+
+  async capture() {
+    const videoo = document.getElementsByTagName('video')[1];
+    const stream = await this.takeScreenshotStream();
+    const result = await new Promise((resolve, reject) => {
+      videoo.onloadedmetadata = () => {
+        // videoo.pause();
+        // videoo.play();
+        const canvas = document.createElement('canvas');
+        canvas.width = this.video.videoWidth;
+        canvas.height = this.video.videoHeight;
+        const context = canvas.getContext('2d');
+        context?.drawImage(
+          stream,
+          0,
+          0,
+          this.video.videoWidth,
+          this.video.videoHeight
+        );
+        this.screenshot = canvas.toDataURL();
+      };
+    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
